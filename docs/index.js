39796f6a8be4545e10d11b611afa7cd7f3cfea1a
lob2018 <@Lob2018>
2026-09-19 18:02:39 +0200

    📚 docs Doxygen

--- docs/index.js
+++ docs/index.js
@@ -2,8 +2,7 @@ var index =
 [
     [ "What this library provides", "index.html#autotoc_md1", null ],
     [ "How to use this library", "index.html#autotoc_md3", null ],
-    [ "How to build", "index.html#autotoc_md4", [
-      [ "Architecture (UML)", "index.html#autotoc_md5", null ]
-    ] ],
+    [ "How to build", "index.html#autotoc_md4", null ],
+    [ "Architecture (UML)", "index.html#autotoc_md5", null ],
     [ "License", "index.html#autotoc_md6", null ]
 ];
